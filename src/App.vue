963fc2e95b0229f68881825871faa7a4f84f3ff4
Dennis Gubbels <34510433+deguviro@users.noreply.github.com>
2021-10-23 14:19:41 +0200

    Implement infobox base

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,34 @@
-<script setup>
-import Reticle from './components/Reticle.vue'
-</script>
-
 <template>
 <div>
-    <Reticle image="./src/assets/image001.jpg"></Reticle>
+    <img src="./res/background.png" class="background">
+    <div class="tourcontainer" id="A">
+        <Reticle image="./src/res/image001.jpg"></Reticle>
+        <InfoBox :textSource=tourA></InfoBox>
+    </div>
 </div>
 </template>
+<script setup>
+import Reticle from './components/Reticle.vue';
+import InfoBox from './components/InfoBox.vue';
+import tourA from './res/tourA.json';
 
+</script>
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
+    /* font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+    color: #2c3e50; */
+}
+
+body {
+    margin: 0;
+}
+
+.tourcontainer {
+    position: absolute;
+    top: 0;
+    left: 0;
 }
 </style>
---
+++ src/components/InfoBox.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="infobox">
+        <div class="textbox">
+            <p class="text-en">{{textEn}}</p>
+            <p class="text-es">{{textEs}}</p>
+        </div>
+        <img class="imagebox"
+            :src="image"
+            >
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: [
+        'textSource'
+    ],
+    data () {
+        return {
+            image: null,
+            textEn: "",
+            textEs: "",
+            hotspot: "idle",
+        }
+    },
+    mounted() {
+        this.textEn = this.$props.textSource[this.hotspot].en;
+        this.textEs = this.$props.textSource[this.hotspot].es;
+        this.image = this.$props.textSource[this.hotspot].img;
+    },
+}
+</script>
+<style scoped>
+.infobox {
+    position: absolute;
+    display: block;
+}
+
+.imagebox {
+    display: block;
+    position: absolute;
+    z-index: 20;
+
+    width: 282px;
+    height: 152px;
+
+    border: 2px solid #eff163;
+}
+
+.textbox {
+    display: block;
+    position: absolute;
+    z-index: 10;
+    top: 124px;
+    left: 33px;
+
+    width: 262px;
+    height: 168px;
+
+    padding: 16px;
+    padding-top: 24px;
+
+    background-color: rgba(0,0,0,0.5);
+
+    font-size: 11pt;
+    font-weight: bold;
+    text-align: left;
+}
+
+.text-en {
+    color: white;
+}
+
+.text-es {
+    color: #eff163;
+}
+
+#A > .infobox {
+    top: 690px;
+    left: 202px;
+}
+</style>
